ed0fd86dbf7951f9808168ac5c46867bc0290cad
LissetteMariePortfolio <89919777+LissetteMariePortfolio@users.noreply.github.com>
2021-11-22 17:34:50 -0500

    Uploading Tablet draft

---
+++ js/site_animations.js
@@ -0,0 +1,46 @@
+
+
+document.getElementById("parent_img1").addEventListener("mouseover", function() {
+    console.log("Hi");
+    document.getElementById("pm_img1").style.opacity = 0;
+});
+document.getElementById("parent_img1").addEventListener("mouseleave", function() {
+    console.log("Bye");
+    document.getElementById("pm_img1").style.opacity = 1;
+});
+
+document.getElementById("parent_img2").addEventListener("mouseover", function() {
+    console.log("Hi");
+    document.getElementById("pm_img2").style.opacity = 0;
+});
+document.getElementById("parent_img2").addEventListener("mouseleave", function() {
+    console.log("Bye");
+    document.getElementById("pm_img2").style.opacity = 1;
+});
+
+document.getElementById("parent_img3").addEventListener("mouseover", function() {
+    console.log("Hi");
+    document.getElementById("pm_img3").style.opacity = 0;
+});
+document.getElementById("parent_img3").addEventListener("mouseleave", function() {
+    console.log("Bye");
+    document.getElementById("pm_img3").style.opacity = 1;
+});
+
+document.getElementById("parent_img4").addEventListener("mouseover", function() {
+    console.log("Hi");
+    document.getElementById("pm_img4").style.opacity = 0;
+});
+document.getElementById("parent_img4").addEventListener("mouseleave", function() {
+    console.log("Bye");
+    document.getElementById("pm_img4").style.opacity = 1;
+});
+
+document.getElementById("parent_img5").addEventListener("mouseover", function() {
+    console.log("Hi");
+    document.getElementById("pm_img5").style.opacity = 0;
+});
+document.getElementById("parent_img5").addEventListener("mouseleave", function() {
+    console.log("Bye");
+    document.getElementById("pm_img5").style.opacity = 1;
+});
